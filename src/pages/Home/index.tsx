cbab6a943c6504a75897cfb2dd053194a34a47b2
Reinaldo Goncalves Ferreira <reinaldo.ferreira@ifood.com.br>
2024-06-28 21:14:09 -0300

    created folders

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import Search from "../../modules/Search";
+import { Container, Content } from "./styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Content>
+        <Search />
+        <h1>Outra coisa</h1>
+      </Content>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: 250px 1fr;
+`;
